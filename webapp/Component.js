a11520ac95de0b4d59a78b29cc2dc48c816d7c61
Chandra Saswata <saswatapablo@gmail.com>
2020-08-22 09:56:28 +0000

    Commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("SapTechEd2019.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
